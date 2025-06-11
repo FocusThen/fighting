df6d611785b276873d1f64c7ca40fe5b3b4d6f75
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-06-11 03:12:30 +0100

    feat: episode 2 done, camera and invisible walls

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -1,12 +1,46 @@
 extends CharacterBody2D
 
-
-@export var health : int
 @export var damage : int
+@export var health : int
 @export var speed : float
 
+@onready var aniamtion_player = $AnimationPlayer
+@onready var character_sprite = $CharacterSprite
 
-func _physics_process(delta: float) -> void:
+
+enum State {IDLE, WALK}
+var state = State.IDLE
+
+func _physics_process(_delta: float) -> void:
+	handle_input()
+	handle_movement()
+	handle_animation()
+	flip_sprites()
+	move_and_slide()
+
+
+func handle_movement():
+	if velocity.length()  == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+
+
+func handle_input():	
 	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = dir * speed
-	move_and_slide()
+	
+
+func handle_animation():
+	if state == State.IDLE:
+		aniamtion_player.play("idle")
+	elif state == State.WALK:
+		aniamtion_player.play("walk")
+		
+		
+
+func flip_sprites():
+	if velocity.x >0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0:
+		character_sprite.flip_h = true
