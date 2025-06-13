0f3c2c6b88b543248ec7e4dc3d823794930abf54
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-06-13 22:41:21 +0100

    feat: episode 3 done, signals Barrel and damage emitters

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -4,12 +4,16 @@ extends CharacterBody2D
 @export var health : int
 @export var speed : float
 
-@onready var aniamtion_player = $AnimationPlayer
-@onready var character_sprite = $CharacterSprite
+@onready var aniamtion_player: AnimationPlayer = $AnimationPlayer
+@onready var character_sprite: Sprite2D = $CharacterSprite
+@onready var damage_emitter: Area2D = $DamageEmitter
 
 
-enum State {IDLE, WALK}
+enum State {IDLE, WALK, ATTACK}
 var state = State.IDLE
+
+func _ready() -> void:
+	damage_emitter.area_entered.connect(on_emit_damage.bind())
 
 func _physics_process(_delta: float) -> void:
 	handle_input()
@@ -19,28 +23,48 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
-func handle_movement():
-	if velocity.length()  == 0:
-		state = State.IDLE
+func handle_movement() -> void:
+	if can_move():
+		if velocity.length()  == 0:
+			state = State.IDLE
+		else:
+			state = State.WALK
 	else:
-		state = State.WALK
+		velocity = Vector2.ZERO
 
-
-func handle_input():	
+func handle_input() -> void:	
 	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = dir * speed
 	
+	if can_attack() and Input.is_action_just_pressed("attack"):
+		state = State.ATTACK
+		
 
-func handle_animation():
+func handle_animation() -> void:
 	if state == State.IDLE:
 		aniamtion_player.play("idle")
 	elif state == State.WALK:
 		aniamtion_player.play("walk")
-		
+	elif state == State.ATTACK:
+		aniamtion_player.play("punch")
 		
 
-func flip_sprites():
+func flip_sprites() -> void:
 	if velocity.x >0:
 		character_sprite.flip_h = false
+		damage_emitter.scale.x = 1
 	elif velocity.x < 0:
 		character_sprite.flip_h = true
+		damage_emitter.scale.x = -1
+		
+func can_move() -> bool:
+	return state == State.IDLE or state == State.WALK
+func can_attack()->bool:
+	return state == State.IDLE or state == State.WALK
+
+func on_action_complete() -> void:
+	state = State.IDLE
+
+func on_emit_damage(damage_receiver: DamageReceiver) -> void:
+	var direction := Vector2.LEFT if damage_receiver.global_position.x < global_position.x else Vector2.RIGHT
+	damage_receiver.damage_received.emit(damage, direction)
--- world.gd
+++ world.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 
-@onready var player: CharacterBody2D = $Player
 @onready var camera: Camera2D = $Camera
+@onready var player: CharacterBody2D = $ActionContainer/Player
 
 func _process(_delta: float) -> void:
 	if player.position.x > camera.position.x:
